08b4d28ec28b1d9cc32e7637276d7c2226db6384
amir.javadikia <83190601+amirjavadikia@users.noreply.github.com>
2022-01-17 21:32:31 +0330

    Create require.php

---
+++ src/require.php
@@ -0,0 +1,12 @@
+<!-- You can require your files here and use this file in everywhere -->
+
+
+<!-- For Example !! -->
+
+
+session_start();
+
+require_once 'autoload.php';
+require_once 'view.php';
+require_once 'db.php';
+require_once  'functions.php';
